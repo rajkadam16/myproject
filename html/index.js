9d482afcea9af9da7cbc4c43748bf510be82043e
rajkadam16 <152425457+rajkadam16@users.noreply.github.com>
2024-01-01 14:08:50 +0530

    feature(spotify_clone);

--- html/index.js
+++ html/index.js
@@ -1,1 +1,8 @@
-console.log("login page")
+function login() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    // Perform authentication logic here
+    // For demonstration purposes, just displaying an alert
+    alert('Username: ' + username + '\nPassword: ' + password);
+  }
